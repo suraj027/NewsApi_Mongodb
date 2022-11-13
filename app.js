00fdd0da359acb46a8f79c1912437b4683f52970
Suraj Satheesh <suraj270301@gmail.com>
2022-11-13 23:40:55 +0530

    file not found error

--- app.js
+++ app.js
@@ -66,11 +66,11 @@ app.use('/', newsRouter)
 app.use('/article', newsRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('File Not Found');
-    err.status = 404;
-    next(err);
-  });
+// app.use(function (req, res, next) {
+//     var err = new Error('File Not Found');
+//     err.status = 404;
+//     next(err);
+//   });
   
   // error handler
   // define as the last app.use callback
